cf3270cec50990a20d4f016eb871e21cc34d793b
Rajan Yadav <rajanyadavsurapur@gmail.com>
2025-10-04 12:36:57 +0530

    my second commit

---
+++ bbb.py
@@ -0,0 +1,39 @@
+import random
+
+item_list=["rock","paper","scissor"]
+comp=random.choice(item_list)
+retry=input("enter play (p)& break (b) the game")
+
+
+while retry=="p":
+    user=input("enter item_list(paper,scissor,rock):")
+    comp=random.choice(item_list)
+    print(f"user choice ={user},computer_choice={comp}") 
+    
+    
+    if comp==user:
+        print("tie")
+           
+    elif comp=="paper":
+        if user=="rock":
+            print("comp win")      
+        else:
+            print("user win ")
+    
+    elif comp=="rock":
+        if user=="scissor":
+            print("comp win")
+        else:
+            print("user win")
+            
+    elif comp=="paper":
+        if user == "scissor":
+            print("user win")
+        elif user=="rock":
+            print("comp win")
+            
+            
+    retry=input("enter play (p)& break (b) the game")
+    
+print("you want to break the game") 
+
