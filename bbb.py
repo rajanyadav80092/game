8ba531ba8cf34a925a7fbecbf6f812585465ac7d
Rajan Yadav <rajanyadavsurapur@gmail.com>
2025-10-04 12:58:08 +0530

    my third commit

--- bbb.py
+++ bbb.py
@@ -2,13 +2,13 @@ import random
 
 item_list=["rock","paper","scissor"]
 comp=random.choice(item_list)
-retry=input("enter play (p)& break (b) the game")
+retry=input("enter play (p)& break (b) the game :")
 
 
 while retry=="p":
     user=input("enter item_list(paper,scissor,rock):")
     comp=random.choice(item_list)
-    print(f"user choice ={user},computer_choice={comp}") 
+    print(f"user choice ={user}\ncomputer_choice={comp}") 
     
     
     if comp==user:
@@ -29,11 +29,12 @@ while retry=="p":
     elif comp=="paper":
         if user == "scissor":
             print("user win")
-        elif user=="rock":
+        else:
             print("comp win")
             
             
-    retry=input("enter play (p)& break (b) the game")
-    
-print("you want to break the game") 
+    retry=input("enter play (p)& break (b) the game : ")
+else:
+       
+    print("you want to break the game") 
 
